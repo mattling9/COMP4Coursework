0e5df0f31f4753ec49e5f71c124666ba37677301
Matt Ling <31477@longroad.ac.uk>
2015-03-11 16:03:16 +0000

    Testing almost done

---
+++ Implementation/Gui/RobustnessEvidence.py
@@ -0,0 +1,13 @@
+stock1 = "twelve"
+stock2 = "three"
+
+stockinorder = "five"
+
+stocktotal = stock1 + stock2
+
+stocknew = stocktotal - stockinorder
+
+print("stock in location1: {0}".format(stock1))
+print("stock in location2: {0}".format(stock2))
+print("total stock: {0}".format(stocktotal))
+print("new stock: {0}".format(stocknew))
