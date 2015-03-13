ae29c2da9b5bbbf0a2cf994e8f9fae33751fa2f3
mattling9 <mattling9@hotmail.co.uk>
2015-03-13 23:46:41 +0000

    Testing Done Manual almost done.

--- Implementation/Gui/setup.py
+++ Implementation/Gui/setup.py
@@ -20,10 +20,14 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 includes = ["atexit","re","matplotlib.backends.backend_tkagg","tkinter","tkinter.filedialog"]
+includefiles = ['C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/app_logo.png','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/close.png','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/Counties.txt','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/CumbriaPostcodes.csv','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/minimize.png','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/SystemLogo.jpg','C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/SystemLogo.png']
 
+build_options = {"includes":includes,
+                 'include_files' : includefiles,
+                 'icon' : 'C:/Users/Matt/Desktop/Github/COMP4Coursework/Implementation/Gui/resources/windows_icon.ico'}
 setup(
         name = application_title,
         version = "0.1",
         description = "Stock control system for the company: Beacon Veternary Center.",
-        options = {"build_exe" : {"includes" : includes}},
+        options = {"build_exe" : build_options},
         executables = [Executable(main_python_file, base = base)])
